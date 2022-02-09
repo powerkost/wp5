7b4758980c811cddad33a9dde634052f4314ea5a
powerkost <65134809+powerkost@users.noreply.github.com>
2022-02-09 17:59:18 +0300

    g

--- js.js
+++ js.js
@@ -1,1 +1,14 @@
-
+function st() {
+    var k = document.getElementById("kol").value;
+    var e = document.getElementById("ed").value;
+    var r = document.getElementById("result");
+    r.innerHTML = k * e;
+    if (k * e === 0) {
+        alert("ноль получается)");
+    }
+}
+window.document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOM fully loaded and parsed");
+    var b = document.getElementById("button");
+    b.addEventListener("click", st);
+});
